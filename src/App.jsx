fdbe99f18795b1543cfb039269bba4e2587a2301
steven <alvaradosteven315@gmail.com>
2022-10-31 13:53:55 -0500

    error founded

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   //to work with the weather api
   const [weather, setWeather] = useState({})
   const [celsius, setCelsius] = useState(true)
+  const [background, setBackground] = useState()
 
   useEffect(() => {
     const success = (pos) => {
@@ -31,23 +32,31 @@ function App() {
     navigator.geolocation.getCurrentPosition(success)
   }, [])
 
-  const changeBackground = (icon) => {
-    switch (icon) {
+  // const changeBackground = () => {
+  //   switch (weather.weather?.[0].icon) {
+  //     case ('01d', '02d', '03d', '04d', '09d', '010d', '011d', '013d', '050d'):
+  //       setBackground('sun2.svg')
+  //       break
+  //     default:
+  //       setBackground('sun.svg')
+  //       break
+  //   }
+  // }
+
+  useEffect(() => {
+    switch (weather.weather?.[0].icon) {
       case ('01d', '02d', '03d', '04d', '09d', '010d', '011d', '013d', '050d'):
-        return 'sun2.svg'
+        setBackground('sun2.svg')
         break
       default:
-        return 'sun.svg'
+        setBackground('sun.svg')
         break
     }
-  }
 
-  // document.body.style = 'background-image: url(src/assets/night2.svg)'
-  document.body.style = `background-image: url(./src/assets/${changeBackground(
-    weather.weather?.[0].icon
-  )})`
+    document.body.style = `background-image: url(./src/assets/${background})`
+  }, [weather])
 
-  console.log(weather.weather?.[0])
+  // console.log(weather.weather?.[0])
 
   return (
     <div className='app'>
